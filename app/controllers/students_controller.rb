040773b2d63c0afb22dd4be3f8fa4be6b2d83862
Caitlyn Dalton <cdalton0610@gmail.com>
2022-06-28 22:09:51 -0400

    Copied files from old repo

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,24 @@
+class StudentsController < ApplicationController
+  def index
+    students = Student.all
+    render json: students.as_json
+  end
+
+  def create
+    student = Student.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      short_bio: params[:short_bio],
+      LinkedIn_URL: params[:LinkedIn_URL],
+      twitter_handle: params[:twitter_handle],
+      personal_blog: params[:personal_blog],
+      online_resume_url: params[:online_resume_url],
+      github_url: params[:github_url],
+      photo: params[:photo],
+    )
+    student.save
+    render json: student.as_json
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/tests" => "tests#index"
+  get "/students" => "students#index"
+  post "/students" => "students#create"
 end
