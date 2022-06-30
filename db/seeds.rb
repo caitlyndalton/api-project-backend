17988dba0e43a2cd02e038c29abc6ee91f324be5
Caitlyn Dalton <cdalton0610@gmail.com>
2022-06-29 21:17:01 -0400

    Added experiences index/routes and hard coded seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
 Student.create(first_name: "Adam", last_name: "Heckathorn", email: "ajheckathorn01@gmail.com", phone_number: "479-305-9290", short_bio: "text", LinkedIn_URL: "https://www.linkedin.com/in/adam-heckathorn-202072210/", twitter_handle: "placeholder", personal_blog: "placeholder", online_resume_url: "placeholder", github_url: "placeholder", photo: "placeholder")
-
 Student.create(first_name: "Caitlyn", last_name: "Dalton", email: "disconerds@gmail.com", phone_number: "678-752-1343", short_bio: "interesting description", LinkedIn_URL: "https://www.linkedin.com/in/caitlyn-dalton", twitter_handle: "@disco_nerds", personal_blog: "placeholder", online_resume_url: "placeholder", github_url: "https://github.com/caitlyndalton", photo: "placeholder")
-
 Student.create(first_name: "Nasser", last_name: "Alqutaim", email: "nasseralqutaim@gmail.com", phone_number: "473-315-9290", short_bio: "some text", LinkedIn_URL: "https://www.linkedin.com/in/nasserqutaim/", twitter_handle: "@jack", personal_blog: "placeholder", online_resume_url: "placeholder", github_url: "placeholder", photo: "placeholder")
+
+Experience.create(start_date: "April 2022", end_date: "July 2022", job_title: "Software Developer", company_name: "Google", details: "example details")
