b36e947cca875b86a335761d52fe4158a34e33a3
Caitlyn Dalton <cdalton0610@gmail.com>
2022-06-29 22:11:32 -0400

    Fixed education controller end

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -4,38 +4,38 @@ class EducationsController < ApplicationController
     render json: educations.as_json
   end
 
-#   def create
-#     education = education.new(
-#       start_date: params[:start_date],
-#       end_date: params[:end_date],
-#       degree: params[:degree],
-#       university_name: params[:university_name],
-#       details: params[:details],
-      
-#     education.save
-#     render json: education.as_json
-#   end
+  #   def create
+  #     education = education.new(
+  #       start_date: params[:start_date],
+  #       end_date: params[:end_date],
+  #       degree: params[:degree],
+  #       university_name: params[:university_name],
+  #       details: params[:details],
 
-#   def show
-#     education = education.find_by(id: params[:id])
-#     render json: education.as_json
-#   end
+  #     education.save
+  #     render json: education.as_json
+  #   end
 
-#   def update
-#     education = education.find_by(id: params[:id])
-#     education.start_date = params[:start_date] || education.start_date
-#     education.end_date = params[:end_date] || education.end_date
-#     education.degree = params[:degree] || education.degree
-#     education.university_name = params[:university_name] || education.university_name
-#     student.details = params[:details] || student.details
+  #   def show
+  #     education = education.find_by(id: params[:id])
+  #     render json: education.as_json
+  #   end
 
-#     education.save
-#     render json: education.as_json
-#   end
+  #   def update
+  #     education = education.find_by(id: params[:id])
+  #     education.start_date = params[:start_date] || education.start_date
+  #     education.end_date = params[:end_date] || education.end_date
+  #     education.degree = params[:degree] || education.degree
+  #     education.university_name = params[:university_name] || education.university_name
+  #     student.details = params[:details] || student.details
 
-#   def destroy
-#     education = education.find_by(id: params[:id])
-#     education.destroy
-#     render json: { message: "Education destroyed." }
-#   end
-# end
+  #     education.save
+  #     render json: education.as_json
+  #   end
+
+  #   def destroy
+  #     education = education.find_by(id: params[:id])
+  #     education.destroy
+  #     render json: { message: "Education destroyed." }
+  #   end
+end
